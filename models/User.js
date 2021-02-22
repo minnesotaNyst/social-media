668f5cbb27f69a351bd5b3304541665c1291121f
minnesotaNyst <71350960+minnesotaNyst@users.noreply.github.com>
2021-02-22 13:27:56 -0600

    changed regex to match prevoius module

--- models/User.js
+++ models/User.js
@@ -1,55 +1,55 @@
-const { Schema, Types, model } = require("mongoose");
+const { Schema, Types, model } = require('mongoose');
 
 const validateEmail = function (email) {
-  var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  return re.test(email);
+	var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+	return re.test(email);
 };
 
 const UserSchema = new Schema(
-  {
-    userName: {
-      type: String,
-      unique: true,
-      required: true,
-      trim: true,
-    },
-    email: {
-      type: String,
-      unique: true,
-      required: true,
-      validate: [validateEmail, "Valid Email Required"],
-      match: [
-        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-        "Please fill a valid email address",
-      ],
-    },
-    thoughts: [
-      {
-        type: Types.ObjectId,
-        ref: "Thought",
-      },
-    ],
-    friends: [
-      {
-        type: Types.ObjectId,
-        ref: "User",
-      },
-    ],
-  },
-  {
-    toJSON: {
-      virtuals: true,
-      getters: true,
-    },
-    // prevents virtuals from creating duplicate of _id as `id`
-    id: false,
-  }
+	{
+		userName: {
+			type: String,
+			unique: true,
+			required: true,
+			trim: true
+		},
+		email: {
+			type: String,
+			unique: true,
+			required: true,
+			validate: [validateEmail, 'Valid Email Required'],
+			match: [
+				/^\S[^\n\r][a-zA-Z0-9\.]+@+\S[^\n\r][a-zA-Z]+[.]+[a-z]+/,
+				'Please fill a valid email address'
+			]
+		},
+		thoughts: [
+			{
+				type: Types.ObjectId,
+				ref: 'Thought'
+			}
+		],
+		friends: [
+			{
+				type: Types.ObjectId,
+				ref: 'User'
+			}
+		]
+	},
+	{
+		toJSON: {
+			virtuals: true,
+			getters: true
+		},
+		// prevents virtuals from creating duplicate of _id as `id`
+		id: false
+	}
 );
 
-UserSchema.virtual("friendsCount").get(function () {
-  return this.friends.length;
+UserSchema.virtual('friendsCount').get(function () {
+	return this.friends.length;
 });
 
-const User = model("User", UserSchema);
+const User = model('User', UserSchema);
 
 module.exports = User;
